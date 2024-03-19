b69f018cbe7dc2c816d83005dcddded01535865c
Johncode248 <125147628+Johncode248@users.noreply.github.com>
2024-03-19 09:30:32 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var tpl *template.Template
+
+func main() {
+	tpl, _ = template.ParseGlob("templates/*.html")
+
+	r := mux.NewRouter()
+	// User functions
+	r.HandleFunc("/", mainPage).Methods("GET")
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Fatal(http.ListenAndServe(":8282", r))
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index3.html", nil)
+}
