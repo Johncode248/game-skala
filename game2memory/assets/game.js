cb4a322dbdd304ee8ad7e239c5ce9637c01e10f4
asekus <stanislawork@gmail.com>
2024-05-13 15:01:42 +0200

    game3up

--- game2memory/assets/game.js
+++ game2memory/assets/game.js
@@ -54,7 +54,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    const emojis = ['🤔', '😢', '😠', '😡', '😳', '😤', '😣', '😞','😭', '😟', '😄','😎','😘','😫','😓','🤪','🤭','🤗']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
---
+++ game3hangman/wordlist.js
@@ -0,0 +1,66 @@
+
+// Check the source code for the questions
+const wordList = [
+    {
+      word: "active listening1",
+      hint: "Without words, but with full focus, what you say, I hear, your voice to me is dear.",
+    },
+    {
+      word: "clear expression2",
+      hint: "A colorless, odorless gas essential for life.",
+    },
+    
+    
+    {
+      word: "Emotional understanding4",
+      hint: "A popular sport played with a spherical ball.",
+    },
+    
+    {
+      word: "Judgment limitation5",
+      hint: "The study of past events and human civilization.",
+    },
+    {
+      word: "Expression of feelings6",
+      hint: "A savory dish consisting of a round, flattened base with toppings.",
+    },
+    {
+      word: "Seeking compromise7",
+      hint: "A genre of music characterized by improvisation and syncopation.",
+    },
+    {
+      word: "Focus on facts9",
+      hint: "A device used to capture and record images or videos.",
+    },
+    {
+      word: "Avoiding aggression10",
+      hint: "A precious gemstone known for its brilliance and hardness.",
+    },
+    
+    {
+      word: "Understanding perspective12",
+      hint: "The systematic study of the structure and behavior of the physical and natural world.",
+    },
+    {
+      word: "Empathy development14",
+      hint: "A human-powered vehicle with two wheels.",
+    },
+    {
+      word: "Open conflict resolution16",
+      hint: "The daily disappearance of the sun below the horizon.",
+    },
+    {
+      word: "Constructive criticism acceptance18",
+      hint: "A popular caffeinated beverage made from roasted coffee beans.",
+    },
+    {
+      word: "Stress management19",
+      hint: "A rhythmic movement of the body often performed to music.",
+    },
+    {
+      word: "Negotiation skills development20",
+      hint: "A vast system of stars, gas, and dust held together by gravity.",
+    },
+    
+  ];
+  
