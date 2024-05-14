996befec8f534fa31162acd1eeb9b97db01321fa
asekus <stanislawork@gmail.com>
2024-05-14 19:09:04 +0200

    wordlist word to lowcase

--- game3hangman/scripts/word-list.js
+++ game3hangman/scripts/word-list.js
@@ -10,53 +10,53 @@ const wordList = [
     
     
     {
-      word: "Emotional understanding",//4
+      word: "emotional understanding",//4
       hint: "In it, emotions you immerse, to understand what your heart rehearses.",
     },
     
     {
-      word: "Judgment limitation",//5
+      word: "judgment limitation",//5
       hint: "No judgments, only facts to run, so communication is like a clock in the sun.",
     },
     {
-      word: "Expression of feelings",//6
+      word: "expression of feelings",//6
       hint: "In words, the strength of feelings lies, for understanding the heart, never to disguise.",
     },
     {
-      word: "Seeking compromise",//7
+      word: "seeking compromise",//7
       hint: "In a difficult situation, this way, each side gives a little, a compromise we play.",
     },
     {
-      word: "Focus on facts",//9
+      word: "focus on facts",//9
       hint: "In facts, there's strength, in emotions, a glow, clear to the eyes like a lamp's light in woe.",
     },
     {
-      word: "Avoiding aggression",//10
+      word: "avoiding aggression",//10
       hint: "No aggression, but decisiveness, speak your mind, to avoid unrestfulness.",
     },
     
     {
-      word: "Understanding perspective", //12
+      word: "understanding perspective", //12
       hint: "Seen through another's eyes, a different path, to understand what guides their heart.",
     },
     {
-      word: "Empathy development", //14
+      word: "empathy development", //14
       hint: "Sooner than shoes on feet, step into others, to become truly empathetic creatures.",
     },
     {
-      word: "Open conflict resolution", //16
+      word: "open conflict resolution", //16
       hint: "In openness and honesty, conflicts solve gently, without noise, noise aplenty.",
     },
     {
-      word: "Constructive criticism acceptance", //18
+      word: "constructive criticism acceptance", //18
       hint: "Accept criticism as a gift, like a noble stone, to walk the path of excellence, always alone.",
     },
     {
-      word: "Stress management", //19
+      word: "stress management", //19
       hint: "The art of managing stress, like playing a violin, for communication to be like music - harmony still.",
     },
     {
-      word: "Negotiation skills development", //20
+      word: "negotiation skills development", //20
       hint: "Negotiation's dance, where both sides play, like a choir singing - an unknown symphony's sway.",
     },
     
